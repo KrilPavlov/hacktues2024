fa7f629c06267d7fdbf262a742132c034bb9d100
Kiril Pavlov <kiril.pavlovv@gmail.coom>
2024-03-14 18:36:44 +0200

    Concatenated qury string for exporting to ardunino

--- HacktuesX/src/ArduinoSonicSensor_serialexport/ArduinoSonicSensor_serialexport.ino
+++ HacktuesX/src/ArduinoSonicSensor_serialexport/ArduinoSonicSensor_serialexport.ino
@@ -44,7 +44,7 @@ void loop() {
     float timeDiff = (finalTime - initialTime) / 1000.0; // Time difference in seconds
     float distanceDiff = finalDistance - initialDistance; // Distance difference in cm
     float speed = abs(distanceDiff) / timeDiff; // Speed in cm/s
-    String direction = distanceDiff < 0 ? "Approaching" : "Leaving";
+    String direction = distanceDiff < 0 ? "True" : "False";
 
     // Output the findings
     //Serial.print("Person Counted. Speed: ");
@@ -54,13 +54,15 @@ void loop() {
     
     peopleCount++; // Increment people count
     String querystring;
-    querystring.concat("speed");
+    querystring.concat("speed=");
     querystring.concat(speed);
     querystring.concat("&");
-    querystring.concat("direction");
+    querystring.concat("direction=");
     querystring.concat(direction);
-    querystring.concat("peopleCount");
-    querystring.concat(peopleCount);
+    querystring.concat("&");
+    querystring.concat("peopleCount=");
+    querystring.concat("1");
+
     Serial.println(querystring);
     // Reset tracking flags
     isTracking = false;
