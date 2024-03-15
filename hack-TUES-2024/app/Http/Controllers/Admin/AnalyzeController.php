1427bea6e33a0c7479d9e49a551aaefaec8b2ebf
Imran Hossain <ihossain@weband.bg>
2024-03-15 22:32:54 +0200

    Sensor Seeder create

--- hack-TUES-2024/app/Http/Controllers/Admin/AnalyzeController.php
+++ hack-TUES-2024/app/Http/Controllers/Admin/AnalyzeController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\SensorData;
 use Illuminate\Http\Request;
+use App\Models\Demo1;
+use Carbon\Carbon;
 
 class AnalyzeController extends Controller
 {
     public function index()
     {
-        $right = SensorData::where('direction', 'True')->get();
-        $left = SensorData::where('direction', 'False')->count();
+        $currentDateTime = Carbon::now();
+        $weekAgoDateTime = $currentDateTime->subDays(7);
+
+        $events = Demo1::where('created_at', '>=', $weekAgoDateTime)->get()->groupBy('event_id');
         // dd($left);
         return view('admin.analyze');
     }
---
+++ hack-TUES-2024/database/seeders/SensorSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Sensor;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SensorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $sensor = new Sensor;
+        // $sensor->start_node = ;
+        // $sensor->end_node = ;
+        // $sensor->lat = ;
+        // $sensor->long = ;
+        // $sensor->save();
+
+    }
+}
