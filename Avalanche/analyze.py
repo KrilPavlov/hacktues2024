7954039669d2d0c037023957cc057c269f6ca9e9
Ivaylo Bozhilov <ibozhilov.elsys@gmail.com>
2024-03-15 10:01:17 +0200

    created report analyzer script

---
+++ Avalanche/analyze.py
@@ -0,0 +1,45 @@
+import openai
+import config
+import base64
+import requests
+
+openai.my_api_key = config.CHATGPT_API_KEY
+
+# OpenAI API Key
+api_key = config.CHATGPT_API_KEY
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "data/reports/15.03/11.jpg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+    "model": "gpt-4-vision-preview",
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": "What’s in this image?"
+          },
+        ]
+      }
+    ],
+    "max_tokens": 4096
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
