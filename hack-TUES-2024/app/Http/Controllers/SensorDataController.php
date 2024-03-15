ebbfd01089aec70d4d0f3251e562dbafdf8d7a96
Imran Hossain <ihossain@weband.bg>
2024-03-15 08:46:57 +0200

    Save to database

--- hack-TUES-2024/app/Http/Controllers/SensorDataController.php
+++ hack-TUES-2024/app/Http/Controllers/SensorDataController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\SensorData;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SensorDataController extends Controller
 {
-    public function index(Request $request){
-        // if($request){
-        //     return redirect()->route('welcome');
-        // }
-        // $data = new SensorData;
+    public function index(Request $request)
+    {
+        $sensore = new SensorData;
+        $data = $request->all();
+
+        if ($data['speed'] != 0) {
+            $sensore->sensore_id = "Ultrasonic sensosre 1";
+            $sensore->speed = $data['speed'];
+            $sensore->direction = $data['direction'];
+            $sensore->save();
+            Log::info($request->all());
+        }
         return $request->all();
     }
-    public function show(){
+    public function show()
+    {
         $data = SensorData::all();
         return $data;
     }
--- hack-TUES-2024/routes/web.php
+++ hack-TUES-2024/routes/web.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
-Route::get('/post', [SensorDataController::class, 'index'])->name('home');
+Route::post('/post', [SensorDataController::class, 'index'])->name('post');
 Route::get('/get', [SensorDataController::class, 'show'])->name('show');
